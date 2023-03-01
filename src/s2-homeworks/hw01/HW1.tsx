7e68e3898d5ea4f783cee8f662e879f7acb73ba5
Artsiom-Chulc <artemorsha@mail.ru>
2023-03-01 20:34:32 +0300

    HW1-ADD

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -12,9 +12,19 @@ import avatar from './avatar.png'
 * 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
 * 5 - сделать стили в соответствии с дизайном
 * */
-
+type UserType = {
+    avatar: string
+    name: string
+}
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: UserType
+    message: {
+        text: string
+        time: string
+    }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
