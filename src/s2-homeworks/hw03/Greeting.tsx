31e1b1b6350d3782e6b2b72395358bc980a97c3a
Artsiom Chults <artemorsha@mail.ru>
2023-03-24 00:14:36 +0300

    fix input styles

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,9 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (? s.errorInput : s.input)
+    const inputClass = `${error ? s.errorInput : s.input}`
+
+    // need to fix with (? s.errorInput : s.input)
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
