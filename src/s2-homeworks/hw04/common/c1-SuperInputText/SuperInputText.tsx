27e3c0457ff0039a2bd001d7425b608a7c971a4e
Artsiom Chults <artemorsha@mail.ru>
2023-03-27 22:34:09 +0300

    fix nix classes

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -48,11 +48,13 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
             onEnter() // то вызвать его
     }
 
-    const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
-    const finalInputClassName = `${s.input}
-     ${error ? s.errorInput : s.superInput}
-     ${className ? s.className : ''}
-     `
+    // const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
+    // const finalInputClassName = `${s.input}
+    //  ${error ? s.errorInput : s.superInput}
+    //  ${className ? s.className : ''}
+
+    const finalSpanClassName = s.error + (spanClassName ? ' ' + spanClassName : '')
+    const finalInputClassName = s.input + (error ? ' ' + s.errorInput : ' ' + s.superInput) + (className ? ' ' + s.className : '')
 
     // const finalSpanClassName = s.error
     //     + (spanClassName ? ' ' + spanClassName : '')
